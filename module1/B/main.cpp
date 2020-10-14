f642f529373d4ce81951dd44c8522e6e68c655cd
vanka857 <vanka857@gmail.com>
2020-10-14 18:18:58 +0300

    Fix code paste missing. Instead of B task I pasted C task previosly

--- module1/B/main.cpp
+++ module1/B/main.cpp
@@ -1,269 +1,91 @@
-//////////////////////////////////////////////////////////////////////////////
-// Problem: 1C.
-//  Шаблон поиска задан строкой длины m, в которой кроме обычных символов
-//  могут встречаться символы “?”.
-//  Найти позиции всех вхождений шаблона в тексте длины n.
-//  Каждое вхождение шаблона предполагает, что все обычные символы совпадают
-//  с соответствующими из текста, а вместо символа “?” в тексте встречается
-//  произвольный символ. Гарантируется, что сам “?” в тексте не встречается.
+///////////////////////////////////////////////////////////////////////////
+// Problem: 1B.
 //
-//  Время работы - O(n + m + Z),
-//  где Z - общее число вхождений подстрок шаблона “между вопросиками”
-//  в исходном тексте. m ≤ 5000, n ≤ 2000000.
+//  Строка называется палиндромом, если она одинаково читается как
+//  слева направо, так и справа налево.
+//  Например, «abba» – палиндром, а «roman» – нет.
 //
-// Пример 1:
-//  Ввод:
-//  ab??aba ababacaba
-//  ababacaba
-//  Вывод:
-//  2
+//  Для заданной строки s длины n (1 ≤ n ≤ 105) требуется подсчитать
+//  число пар (i, j), 1 ≤ i < j ≤ n, таких что подстрока s[i..j]
+//  является палиндромом.
 //
-// Пример 2
-//  Ввод:
-//  aa??bab?cbaa? aabbbabbcbaabaabbbabbcbaab
-//  aabbbabbcbaabaabbbabbcbaab
-//  Вывод:
-//  0 13
-//////////////////////////////////////////////////////////////////////////////
+// Формат ввода:
+//  Входной файл содержит одну строку s длины n, состоящюю из маленьких
+//  латинских букв.
+//
+// Формат вывода:
+//  В выходной файл выведите искомое число подстрок-палиндромов.
+///////////////////////////////////////////////////////////////////////////
 
-#include <vector>
 #include <iostream>
-#include <cstdint>
 #include <string>
+#include <cstdint>
+#include <vector>
 
 
-class Trie {
-private:
+int64_t calcPalindroms(const std::string & data) {
+    int64_t n = data.length();
 
-    struct Node {
-        static const uint32_t K = 26;
+    std::vector<int64_t> d1(n);
 
-        std::vector<int> next;
-        std::vector<int> go;
+    int64_t l = 0, r = -1;
 
-        void make_resizing(const uint32_t alphabet_size) {
-            next.resize(alphabet_size, -1);
-            go.resize(alphabet_size, -1);
+    for (int64_t i = 0; i < n; ++i) {
+        int64_t k = 1;
+        if (i <= r) {
+            k = std::min(d1[l + r - i], r - i + 1);
+        }
+        while (i + k < n && i - k >= 0 && data[i + k] == data[i - k]) {
+            ++k;
         }
 
-        explicit Node(const uint32_t alphabet_size = K){
-            make_resizing(alphabet_size);
-        }
-
-        Node (int parent, int char_to_parent, const uint32_t alphabet_size = K) : parent(parent), char_to_parent(char_to_parent){
-            make_resizing(alphabet_size);
-        }
-
-        bool is_terminal = false;
-        std::vector<int> terminal_pattern_number;
-        int suffix_link = -1;
-        int up_link = -1;
-
-        char char_to_parent = -1;
-        int parent = -1;
-
-    } typedef Node;
-
-    struct Template_t {
-        std::string template_text;
-        uint32_t begin_pos = 0;
-
-        std::string toStr() const {
-            std::string res;
-            res += "template: " + template_text + " pos: ";
-            res += std::to_string(begin_pos);
-            res += "|";
-
-            return res;
-        }
-
-        size_t length() const {
-            return template_text.length();
-        }
-    };
-
-    void addTemplate(const Template_t & template_t, int terminal_number = 0) {
-        int v = 0;
-        for (int i = 0; i < template_t.length(); ++i) {
-            char c = template_t.template_text[i] - 'a';
-            if (trie[v].next[c] == -1) {
-                trie[v].next[c] = trie.size();
-                trie.emplace_back(Node(v, c));
-            }
-            v = trie[v].next[c];
-        }
-        trie[v].is_terminal = true;
-        trie[v].terminal_pattern_number.emplace_back(terminal_number);
-    }
-
-    static std::vector<Template_t> makeTemplates(const std::string & template_t) {
-        std::vector<Template_t> result;
-
-        Template_t temp;
-
-        int j = 0;
+        d1[i] = k - 1;
         
-        for (uint32_t i = 0; i < template_t.length(); ++i) {
-
-            if (template_t[i] == '?' || i == template_t.length()) {
-                if(!temp.template_text.empty()) {
-                    temp.begin_pos = i - temp.length();
-                    addTemplate(temp, j++);
-                }
-
-                temp.template_text = "";
-            }
-            else {
-                temp.template_text += template_t[i];
-            }
+        if (i + k - 1 > r) {
+            l = i - k + 1;
+            r = i + k - 1;
         }
+    }
 
-        if(!temp.template_text.empty()) {
-            temp.begin_pos = template_t.length() - temp.length();
-            addTemplate(temp, j++);
+    std::vector<int64_t> d2(n);
+
+    l = 0, r = -1;
+
+    for (int64_t i = 0; i < n; ++i) {
+        int64_t k = 0;
+        if (i <= r){
+            k = std::min(d2[l+r-i+1], r-i+1);
         }
-
-        return result;
-    }
-
-    
-
-    void make(const std::vector<Template_t> & templates) {
-        trie.resize(1);
-
-        int i = 0;
-
-//        for (const auto & template_t : templates) {
-//            addTemplate(template_t, i++);
-//        }
-    }
-
-    int getLink (int v) {
-        if (trie[v].suffix_link == -1) {
-            if (v == 0 || trie[v].parent == 0) {
-                trie[v].suffix_link = 0;
-            }
-            else {
-                trie[v].suffix_link = go(getLink(trie[v].parent), trie[v].char_to_parent);
-            }
+        while (i + k < n && i - k - 1 >= 0 && data[i + k] == data[i - k - 1]) {
+            ++k;
         }
-        return trie[v].suffix_link;
-    }
+        
+        d2[i] = k;
 
-    int go (int v, char c) {
-        if (trie[v].go[c] == -1) {
-            if (trie[v].next[c] != -1) {
-                trie[v].go[c] = trie[v].next[c];
-            } else if (v == 0) {
-                trie[v].go[c] = 0;
-            }
-            else {
-                trie[v].go[c] = go(getLink(v), c);
-            }
+        if (i + k - 1 > r) {
+            l = i - k;
+            r = i + k - 1;
         }
-
-        return trie[v].go[c];
     }
 
-    int getUp(int v) {
-        if (trie[v].up_link == -1) {
-            auto w = getLink(v);
+    int64_t result = 0;
 
-            if (trie[w].is_terminal || w == 0) {
-                trie[v].up_link = w;
-            }
-            else {
-                trie[v].up_link = getUp(w);
-            }
-        }
-        return trie[v].up_link;
+    for (auto i : d1) {
+        result += i;
+    }
+    for (auto i : d2) {
+        result += i;
     }
 
-    std::vector<Node> trie;
-    std::vector<Template_t> templates;
-    std::string template_t;
-
-public:
-
-    explicit Trie(const std::string & template_t) {
-        templates = makeTemplates(template_t);
-        trie.reserve(template_t.length() + 1);
-
-        this->template_t = template_t;
-        make(templates);
-    }
-
-    std::vector<int> process(const std::string & text) {
-        std::vector<int> result;
-        std::vector<int> counter(text.length());
-
-        if (text.length() < template_t.length())
-            return result;
-
-        int cur = 0;
-
-        for (int i = 0; i < text.length(); ++i) {
-            char c = text[i] - 'a';
-
-            cur = go(cur, c);
-
-            auto a = cur;
-            while (a != 0) {
-                if (trie[a].is_terminal) {
-                    for (auto number : trie[a].terminal_pattern_number) {
-                        int pos = i + 1 - templates[number].length() - templates[number].begin_pos;
-
-                        //std::cout << "Want to finish " << number << " template at " << pos << std::endl;
-
-                        if (pos >= 0)
-                            counter[pos]++;
-                    }
-                }
-                a = getUp(a);
-            }
-        }
-
-        for (int i = 0; i < counter.size() - template_t.length() + 1; ++i) {
-            if (counter[i] == templates.size()) {
-                result.push_back(i);
-            }
-        }
-
-        return result;
-    }
-
-};
-
-
-template <typename T>
-void printVector(const std::vector<T> & data) {
-    for (const auto & i : data) {
-        std::cout << i.toStr() << " ";
-    }
-    std::cout << std::endl;
+    return result;
 }
-
-void printVector(const std::vector<int> & data) {
-    for (const auto & i : data) {
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
-}
-
 
 int main() {
-    std::string template_t, text;
+    std::string input;
 
-    std::cin >> template_t >> text;
+    std::cin >> input;
 
-    Trie trie(template_t);
-    printVector(trie.process(text));
+    std::cout << calcPalindroms(input);
 
     return 0;
 }
-
-// aa?aa?aa?? aabaacaadd 0
-// a?a?a?? abacabacd 0 2
-// abac??ac abacabac 0
-// ab??aba? ababacabaa 2
-// ab??aba?ab ababacabacab 2
