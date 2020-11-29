c22e070291167508e98fde2528abfb221ccb884f
vanka857 <vanka857@gmail.com>
2020-11-29 21:30:00 +0300

    2st issue iteration fixed: 1. Make initializer list constructor. 2. Create & use count() func.

--- module1/D/main.cpp
+++ module1/D/main.cpp
@@ -2,12 +2,15 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <functional>
 
 class SuffixArray {
 private:
     const size_t ALPHABET_SIZE = 256;
-    size_t size_;
+
+    // The order of fields is important (member initializing list)
     std::string text_;
+    size_t size_;
     std::vector<int> suffix_array_;
     std::vector<int> equivalence_classes_;
     std::vector<int> LCP_;
@@ -23,31 +26,38 @@ public:
     size_t calcDifferentSubstrings();
 };
 
-SuffixArray::SuffixArray(std::string text) : text_(std::move(text)){
+// size_ = text_.size() + 1 because in constructor body we increase text_, but it executed finally
+SuffixArray::SuffixArray(std::string text) : text_(std::move(text)), size_(text_.size() + 1), suffix_array_(size_), equivalence_classes_(size_){
     text_ += '$';
-    size_ = text_.size();
-    suffix_array_.resize(size_);
-    equivalence_classes_.resize(size_);
 
     fillSuffixArray();
+}
+
+std::vector<int> count(const size_t & alphabet_size, const std::function<int (size_t i)>& getI, const size_t & src_size) {
+    // функция расчёта позиции элементов, получаемых из getI(size_t i) в сортировке подсчётом
+
+    std::vector<int> counter(alphabet_size, 0);
+
+    for (size_t i = 0; i < src_size; ++i) {
+        ++counter[getI(i)];
+    }
+    for (size_t i = 1; i < alphabet_size; ++i) {
+        counter[i] += counter[i - 1];
+    }
+
+    return std::move(counter);
 }
 
 void SuffixArray::fillSuffixArray() {
     // Сначала отсортируем подстроки по 0му символу (нумерация с нуля)
 
-    std::vector<int> counter(ALPHABET_SIZE, 0); //  счетчик вхождений сортировки подсчётом
-    for (auto ch : text_) {
-        ++counter[ch];
-    }
-    for (size_t i = 1; i < ALPHABET_SIZE; ++i) {
-        counter[i] += counter[i - 1];
-    }
+    // Получаем позиции символов из text и делаем сортировку подсчётом, точнее записываем,
+    // какая строка будет стоять на i-ой позиции в suffix_array
+    auto startsWith = count(ALPHABET_SIZE, [this](size_t i){ return text_[i]; }, size_);
     for (size_t i = 0; i < size_; ++i) {
-        --counter[text_[i]];
+        --startsWith[text_[i]];
 
-        // сортируем подстроки по возрастанию 0го символа? точнее записываем,
-        // какая строка будет стоять на i-ой позиции в suffix_array
-        suffix_array_[counter[text_[i]]] = i;
+        suffix_array_[startsWith[text_[i]]] = i;
     }
 
     // Потом заполним массив классов эквивалентностей
@@ -63,7 +73,7 @@ void SuffixArray::fillSuffixArray() {
 
     // Отсортируем в цикле подстроки длинной 2^k > size_
 
-    for (size_t i = 0, k = 1; ; ++i, k = 1u << i) {
+    for (size_t k = 1; k <= size_; k <<= 1u) {
 
         // Если на очередном шаге сортировки получилось, что все подстроки находятся в разных классах эквивалентности,
         // Значит от сортировки внутри одного класса экв-ти (если учитывать следующие 2^(k - 1) символов)
@@ -81,25 +91,22 @@ void SuffixArray::fillSuffixArray() {
 void SuffixArray::sortByFirst2PowKSymbols(size_t k) {
     // На этом шаге сортировки выходным результатом будут отсортированные строки длины 2^k символов
 
+    // Эта функция сортирует подстроки text_[2^(k - 1) ... 2^k - 1] (нумерация с нуля) и записывает результат в suffix_array_,
+    // используя уже отсортированные строки text_[0 ... 2^(k - 1) - 1]
+
     std::vector<int> suffix_array_1(size_);
     std::vector<int> equivalence_classes_1(size_);
 
+    // "закольцовываем" подстроки
     for (size_t i = 0; i < size_; ++i) {
-        // сортируем подстроки text_[2^(k - 1) ... 2^k - 1] (нумерация с нуля) и записываем результат в suffix_array_1
-        // для этого используем уже отсортированные строки text_[0 ... 2^(k - 1) - 1]
-        // и при необходимости "закольцовываем" подстроки
         suffix_array_1[i] = (suffix_array_[i] - k + size_) % size_;
     }
-    // сортируем подсчётом
-    std::vector<int> counter(n_of_classes_, 0);
-    for (size_t i = 0; i < size_; ++i) {
-        ++counter[equivalence_classes_[suffix_array_1[i]]];
-    }
-    for (size_t i = 1; i < n_of_classes_; ++i) {
-        counter[i] += counter[i - 1];
-    }
+
+    // Получаем позиции начала классов эквивалентностей подстрок из suffix_array_1
+    // и сортируем подстроки из suffix_array_1 подсчётом (записываем в suffix_array_).
+    auto startsWith = count(n_of_classes_, [this, suffix_array_1](size_t i){ return equivalence_classes_[suffix_array_1[i]];}, size_);
     for (size_t i = size_; i > 0; --i) {
-        auto & j = counter[equivalence_classes_[suffix_array_1[i - 1]]];
+        auto & j = startsWith[equivalence_classes_[suffix_array_1[i - 1]]];
         --j;
         suffix_array_[j] = suffix_array_1[i - 1];
     }
@@ -126,7 +133,6 @@ void SuffixArray::sortByFirst2PowKSymbols(size_t k) {
     }
 
     // Сохраняем новый классы эквивалентности подстрок
-
     equivalence_classes_ = std::move(equivalence_classes_1);
 }
 
